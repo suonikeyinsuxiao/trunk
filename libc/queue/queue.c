80930d92b764d3d91f8f4d72d3390e01174373ba
frank <junfu0903@aliyun.com>
2015-12-09 22:31:39 +0800

    修改队列head和tail越界问题

--- libc/queue/queue.c
+++ libc/queue/queue.c
@@ -1,59 +1,76 @@
 #include <stdio.h>
+#define SIZE 5
 
-#define QUEUE_SIZE 5
-
-static int head = 0;
-static int tail = 0;
-static int queue[QUEUE_SIZE];
+int queue[SIZE] = {0};
+int head = 0;
+int tail = 0;
 
 int is_empty()
 {
-	return head==tail;
+	return head == tail;
 }
 
 int is_full()
 {
-	return (tail+1)%QUEUE_SIZE == head;
+	return (tail+1)%SIZE == head;
 }
 
-int enqueue(int value)
+int enqueue(int val)
 {
 	if (is_full())
 	{
-		printf("queue full...\n");	
-		return 0;
+		printf("queue is full...\n");
+		return 0; 
 	}
-	queue[tail++] = value;
+
+	queue[tail] = val;
+	tail = (tail+1)%SIZE;
 	return 1;
 }
 
-int dequeue(int *value)
+int dequeue(int *val)
 {
 	if (is_empty())
 	{
-		printf("queue empty...\n");	
+		printf("queue is empty...\n");
 		return 0;
 	}
-	
-	*value = queue[head++];
+
+	*val = queue[head];
+	head = (head+1)%SIZE;
 	return 1;
 }
 
 int main()
 {
 	int i = 0;
-	for (i = 0; i< QUEUE_SIZE; i++)
+	int val = 0;
+
+	for (i = 0; i < SIZE; i++)
 	{
 		if (enqueue(i))
-			printf("%d	", i/*queue[tail-1]*/);
+			printf("queue[%d]=%d\n", i, queue[i]);
 	}
 
-	int value;
-	for (i = 0; i< QUEUE_SIZE; i++)
+	for (i = 0; i < SIZE; i++)
 	{
-		if (dequeue(&value))
-			printf("%d	", value);
+		if (dequeue(&val))	
+			printf("val=%d\n", val);
 	}
 
+	for (i = 0; i < SIZE; i++)
+	{
+		if (enqueue(i))
+			printf("queue[%d]=%d\n", i, queue[i]);
+	}
+
+	for (i = 0; i < SIZE; i++)
+	{
+		if (dequeue(&val))	
+			printf("val=%d\n", val);
+	}
+
+	printf("head=%d, tail=%d\n", head, tail);
 	return 0;
 }
+
